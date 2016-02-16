92ba11b7a4ca79fc982544c19a6262a0284d6420
surunzi <surunzi@foxmail.com>
2016-02-16 16:25:41 +0800

    Fix: 404

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebVN-demo
+
+Click [Here](http://surunzi.github.io/WebVN-demo/) to see it in action!!
---
+++ config/core.js
@@ -0,0 +1,53 @@
+webvn.extend('config', function (exports) {
+
+    exports.script = {
+        scenarios: [
+            'init',
+            'first'
+        ],
+        path: 'scenario/',
+        extension: 'wvn'
+    };
+
+    exports.system = {
+        title: 'WebVN'
+    };
+
+    exports.ui = {
+        container: '#webvn',
+        defaultTpl: '<div class="center"><div id="webvn"></div></div>',
+        width: 1280,
+        height: 720,
+        autoResize: true,
+        lang: 'zh'
+    };
+
+    exports.log = {
+        colors: {
+            info: '#07a',
+            error: '#eb6864',
+            warn: '#f9c621'
+        }
+    };
+
+    exports.media = {
+        bgm: {
+            path: 'asset/bgm/',
+            extension: 'ogg'
+        },
+        se: {
+            path: 'asset/sound/',
+            extension: 'ogg'
+        },
+        vo: {
+            path: 'asset/voice/',
+            extension: 'ogg'
+        }
+    };
+
+    exports.canvas = {
+        lumaPath: 'engine/img/luma/',
+        lumaExtension: 'png'
+    };
+
+});
